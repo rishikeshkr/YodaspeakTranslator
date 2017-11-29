41d8d14246ca68f785afb7b5496f51e1422dc1dc
rishikeshkr <34095950+rishikeshkr@users.noreply.github.com>
2017-11-29 18:09:39 +0530

    API Layer
    It contains api endpoint to call yoda speak translator service.

---
+++ API.YodaSpeak/YodaTranslator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace API.YodaSpeak
+{
+    public static class YodaTranslator
+    {
+        public static string YodaSpeak(string inputString)
+        {
+            string result = string.Empty;
+            try
+            {
+                if (!string.IsNullOrEmpty(inputString))
+                {
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri(YodaSpeakRes.YodaSpeakEndPointURL);
+                    client.DefaultRequestHeaders.Add(YodaSpeakRes.MashapeKey,YodaSpeakRes.YodaSpeakAPI);
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(YodaSpeakRes.EnpointHeader));
+                    HttpResponseMessage response = client.GetAsync(YodaSpeakRes.MashapeParam + inputString.ToString()).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var clientresult = response.Content.ReadAsStringAsync().Result;
+                        return result = clientresult;
+                    }
+                    else
+                    {
+                        return result;
+                    }
+
+                }
+                else
+                {
+                    return result;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return result;
+            }
+        }
+    }
+}
